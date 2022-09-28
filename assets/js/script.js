bf6b4b44c179ea1a819c1a2ed75bcbb25e9b9818
seanj06 <seanj06@hotmail.com>
2022-09-28 18:55:19 +0000

    Add questions array

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,41 @@ const questionText = document.getElementById('question');
 const answerButtons = document.getElementById('answer-buttons');
 const score = document.getElementById('score');
 
+// Quiz questions
+
+const questions = [
+    {
+        question: "Which player scored the fastest hat-trick in the Premier League?",
+        answers: [{
+                text: "Sadio Mane",
+                correct: true
+            },
+            {
+                text: "Christiano Ronaldo",
+                correct: false
+            },
+            {
+                text: "Ruud Van Nistlerooy",
+                correct: false
+            },
+            {
+                text: "Wayne Rooney",
+                correct: false
+            }
+        ]
+    },
+    {
+        question: "The fastest goal scored in Premier League history came in 7.69 seconds. Who scored it?",
+        answers: [
+            {text: "Mohamed Salah", correct: false},
+            {text: "Didier Drogba", correct: false},
+            {text: "Shane Long", correct: true},
+            {text: "Troy Deeney", correct: false},
+        ]
+    }
+
+]
+
 
 // Runs startGame function when start button is clicked
 
@@ -33,7 +68,6 @@ function startGame() {
         nextButton.classList.remove('hide');
         questionText.classList.remove('hide');
         answerButtons.classList.remove('hide');
-
     } else {
         alert("Please enter a valid username with 3 characters or more.")
     }
