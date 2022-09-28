754f1b26ecc7761eb4a10428026813523086acef
seanj06 <seanj06@hotmail.com>
2022-09-28 18:29:43 +0000

    Add comments to HTML, CSS and Javacript files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// DOM variables
+
 const startButton = document.getElementById('start-btn');
 const userNameText = document.getElementById('username').value;
 const userName = document.getElementById('username');
@@ -9,8 +11,13 @@ const answerButtons = document.getElementById('answer-buttons');
 const score = document.getElementById('score');
 const restartButton = document.getElementById('restart-btn');
 
+// Runs startGame function when start button is clicked
+
 startButton.addEventListener('click', startGame);
 
+/**
+ * Gets value of input username,checks if it is 3 characters or more and hides all relevant elements to start game
+ */
 function startGame() {
     let userNameText = document.getElementById('username').value;
     if (userNameText.length >= 3) {
