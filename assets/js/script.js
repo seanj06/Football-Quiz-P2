ba8719218a761c389a00165057c24e5c7fc2d690
seanj06 <seanj06@hotmail.com>
2022-09-28 18:37:08 +0000

    Add next button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,19 @@
 // DOM variables
 
+// Control Buttons
 const startButton = document.getElementById('start-btn');
+const restartButton = document.getElementById('restart-btn');
+const nextButton = document.getElementById('next-btn');
+// Input Field
 const userNameText = document.getElementById('username').value;
 const userName = document.getElementById('username');
 const inputField = document.getElementById('name-input');
 const nameLabel = document.getElementById('name-label');
-
+// Quiz Container
 const questionText = document.getElementById('question');
 const answerButtons = document.getElementById('answer-buttons');
 const score = document.getElementById('score');
-const restartButton = document.getElementById('restart-btn');
+
 
 // Runs startGame function when start button is clicked
 
@@ -26,9 +30,10 @@ function startGame() {
         userName.classList.add('hide');
         nameLabel.classList.add('hide');
         score.classList.remove('hide');
-        restartButton.classList.remove('hide');
+        nextButton.classList.remove('hide');
         questionText.classList.remove('hide');
         answerButtons.classList.remove('hide');
+
     } else {
         alert("Please enter a valid username with 3 characters or more.")
     }
