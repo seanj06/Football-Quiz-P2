ee69d5fdd9782d7f7840d7a55f5daf4d981920ae
seanj06 <seanj06@hotmail.com>
2022-09-28 19:10:56 +0000

    Declare and add shufflequestion, shuffleanswer and current question variables to start game function

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,11 +13,14 @@ const nameLabel = document.getElementById('name-label');
 const questionText = document.getElementById('question');
 const answerButtons = document.getElementById('answer-buttons');
 const score = document.getElementById('score');
+// Variables needed to display questions
+let shuffleQuestions;
+let currentQuestion;
+let shuffleAnswers;
 
 // Quiz questions
 
-const questions = [
-    {
+const questions = [{
         question: "Which player scored the fastest hat-trick in the Premier League?",
         answers: [{
                 text: "Sadio Mane",
@@ -39,13 +42,200 @@ const questions = [
     },
     {
         question: "The fastest goal scored in Premier League history came in 7.69 seconds. Who scored it?",
-        answers: [
-            {text: "Mohamed Salah", correct: false},
-            {text: "Didier Drogba", correct: false},
-            {text: "Shane Long", correct: true},
-            {text: "Troy Deeney", correct: false},
+        answers: [{
+                text: "Mohamed Salah",
+                correct: false
+            },
+            {
+                text: "Didier Drogba",
+                correct: false
+            },
+            {
+                text: "Shane Long",
+                correct: true
+            },
+            {
+                text: "Troy Deeney",
+                correct: false
+            },
         ]
-    }
+    },
+    {
+        question: "Which country won the first ever World Cup in 1930?",
+        answers: [{
+                value: "Brazil",
+                correct: false
+            },
+            {
+                value: "Spain",
+                correct: false
+            },
+            {
+                value: "Uruguay",
+                correct: true
+            },
+            {
+                value: "Mexico",
+                correct: false
+            },
+        ]
+
+    },
+
+    {
+        question: "The record number of World Cup goals is 16, scored by who?",
+        answers: [{
+                value: "Miraslav Klose",
+                correct: true
+            },
+            {
+                value: "Pele",
+                correct: false
+            },
+            {
+                value: "Thomas Muller",
+                correct: false
+            },
+            {
+                value: "Lionel Messi",
+                correct: false
+            },
+        ]
+
+    },
+
+    {
+        question: "Which Spanish club's nickname is Los Colchoneros, which translates to English as 'The Mattress Makers'?",
+        answers: [{
+                value: "Espanyol",
+                correct: false
+            },
+            {
+                value: "Real Vallodid",
+                correct: false
+            },
+            {
+                value: "Valencia",
+                correct: false
+            },
+            {
+                value: "Athletico Madrid",
+                correct: true
+            },
+        ]
+
+    },
+
+    {
+        question: "Messi has spent his entire professional career at Barcelona, but what was his schoolboy team?",
+        answers: [{
+                value: "Newells Old Boys",
+                correct: true
+            },
+            {
+                value: "River Plate",
+                correct: false
+            },
+            {
+                value: "Boca Juniors",
+                correct: false
+            },
+            {
+                value: "Chacarita Juniors",
+                correct: false
+            },
+        ]
+
+    },
+
+    {
+        question: "Who is the only player to win the Champions League with three different clubs?",
+        answers: [{
+                value: "Clarence Seedorf",
+                correct: true
+            },
+            {
+                value: "Zlatan Ibrahimovic",
+                correct: false
+            },
+            {
+                value: "Kaka",
+                correct: false
+            },
+            {
+                value: "Luis Figo",
+                correct: false
+            },
+        ]
+
+    },
+
+    {
+        question: "Which team was the first from the UK to win the European Cup?",
+        answers: [{
+                value: "Manchester United",
+                correct: false
+            },
+            {
+                value: "Celtic",
+                correct: true
+            },
+            {
+                value: "Nottingham Forrest",
+                correct: false
+            },
+            {
+                value: "Liverpool",
+                correct: false
+            },
+        ]
+
+    },
+
+    {
+        question: "After Juventus, AC Milan and Inter, which team has won the most Serie A titles?",
+        answers: [{
+                value: "Cagliari",
+                correct: false
+            },
+            {
+                value: "Napoli",
+                correct: false
+            },
+            {
+                value: "Genoa",
+                correct: true
+            },
+            {
+                value: "Lazio",
+                correct: false
+            },
+        ]
+
+    },
+
+    {
+        question: "Which outfield player appeared in the Champions League final in three different decades?",
+        answers: [{
+                value: "Ryan Giggs",
+                correct: true
+            },
+            {
+                value: "Paul Scholes",
+                correct: false
+            },
+            {
+                value: "Wayne Rooney",
+                correct: false
+            },
+            {
+                value: "Roy Keane",
+                correct: false
+            },
+        ]
+
+    },
+
 
 ]
 
@@ -68,7 +258,16 @@ function startGame() {
         nextButton.classList.remove('hide');
         questionText.classList.remove('hide');
         answerButtons.classList.remove('hide');
+        // Shuffles the questions to give a random array
+        shuffleQuestions = questions.sort(() => Math.random() - .5);
+        // Sorts the answers array to match up with the shuffled question
+        shuffleAnswers = shuffleQuestions[0].answers.sort(() => Math.random() - .5);
+        currentQuestion = 0;
+        displayQuestion();
     } else {
         alert("Please enter a valid username with 3 characters or more.")
     }
 }
+
+
+function displayQuestion()
